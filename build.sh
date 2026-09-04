31ffc70ca356518494ccc985836c4d531b7057d7
Ashu Tandon <ashutandon00@gmail.com>
2026-09-05 02:16:10 +0530

    demo data seeding

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
-# Render build command. Also works fine as a generic "install + prep" script
-# for any host that gives you a shell before starting the process.
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+
 python manage.py migrate
+python manage.py collectstatic --noinput
+
+if [ "${SEED_DEMO:-false}" = "true" ]; then
+    python manage.py seed_demo
+fi
